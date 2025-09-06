12c3b98d64bd474a3ea0a7ab4d5385635988e126
Angkon Kar <angkon310@gmail.com>
2025-09-06 21:30:51 +0600

    Added JS code file

---
+++ script.js
@@ -0,0 +1,5 @@
+// Initialize Lucide icons
+lucide.createIcons();
+
+// Dynamically update the current year in the footer
+document.getElementById('current-year').textContent = new Date().getFullYear();
